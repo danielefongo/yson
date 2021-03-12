56d4b3ba96a4cbf96613950406b9c487b27c587c
danielefongo <danielefongo@gmail.com>
2021-03-12 21:08:38 +0100

    add simple implementation

--- lib/graphql_builder.ex
+++ lib/graphql_builder.ex
@@ -1,18 +1,24 @@
 defmodule GraphqlBuilder do
-  @moduledoc """
-  Documentation for `GraphqlBuilder`.
-  """
+  @moduledoc false
 
-  @doc """
-  Hello world.
+  def build(data), do: inner_build(data, 0) |> Enum.join("\n")
 
-  ## Examples
-
-      iex> GraphqlBuilder.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def inner_build(data, indentation) when is_map(data) do
+    inner = data |> Enum.map(fn d -> inner_build(d, indentation + 1) end)
+    indent(["{", inner, "}"], indentation)
   end
+
+  def inner_build({key, value}, indentation) when is_nil(value) do
+    camelized_key = key |> Atom.to_string() |> Recase.to_camel()
+    indent([camelized_key], indentation)
+  end
+
+  def inner_build({key, value}, indentation) when is_map(value) do
+    inner = value |> Enum.map(fn d -> inner_build(d, indentation) end)
+    indent(["#{key} {", inner, "}"], indentation)
+  end
+
+  defp indent(data, indentation), do: data |> List.flatten() |> Enum.map(fn d -> indent(indentation) <> d end)
+
+  defp indent(indentation), do: String.duplicate("  ", indentation)
 end
--- test/graphql_builder_test.exs
+++ test/graphql_builder_test.exs
@@ -1,8 +1,19 @@
 defmodule GraphqlBuilderTest do
   use ExUnit.Case
-  doctest GraphqlBuilder
 
-  test "greets the world" do
-    assert GraphqlBuilder.hello() == :world
+  test "build empty inner query" do
+    assert GraphqlBuilder.build(%{}) == "{\n}"
+  end
+
+  test "build query with root fields" do
+    assert GraphqlBuilder.build(%{name: nil, surname: nil}) == "{\n  name\n  surname\n}"
+  end
+
+  test "build query with nested fields" do
+    assert GraphqlBuilder.build(%{root: %{nested: nil}}) == "{\n  root {\n    nested\n  }\n}"
+  end
+
+  test "build query using camel case" do
+    assert GraphqlBuilder.build(%{full_name: nil}) == "{\n  fullName\n}"
   end
 end
