7085d3200fa593ab6e69dbf5b2be915dcf9edd89
danielefongo <danielefongo@gmail.com>
2021-03-13 02:05:16 +0100

    add macros to define a schema

--- lib/graphy.ex
+++ lib/graphy.ex
@@ -3,7 +3,50 @@ defmodule Graphy do
 
   defmacro __using__(_) do
     quote do
-      def query, do: %{root: %{person: [name: nil]}}
+      require Graphy
+      import Graphy
     end
   end
+
+  defmacro field(data) do
+    quote do
+      {unquote(data), nil}
+    end
+  end
+
+  defmacro field(name, _opts \\ [], do: body) do
+    body = cleanup(body)
+    quote do
+      {unquote(name), Enum.into(unquote(body), %{})}
+    end
+  end
+
+  defmacro interface(name, _opts \\ [], do: body) do
+    body = cleanup(body)
+    quote do
+      {unquote(name), unquote(body)}
+    end
+  end
+
+  defmacro object(_name, _opts \\ [], do: body) do
+    body = cleanup(body)
+    quote do
+      def query, do: Enum.into(unquote(body), %{})
+    end
+  end
+
+  defp cleanup(body) do
+    body
+    |> ast_to_list()
+    |> filter_valid_macros()
+  end
+
+  defp ast_to_list(body) do
+    case body do
+      {:__block__, _, content} -> content
+      other -> [other]
+    end
+  end
+
+  defp filter_valid_macros(list), do: Enum.filter(list, fn {func_name, _, _} -> Enum.member?([:field, :interface], func_name) end)
 end
--- test/graphy_test.exs
+++ test/graphy_test.exs
@@ -3,9 +3,21 @@ defmodule GraphyTest do
 
   defmodule Sample do
     use Graphy
+
+    object :sample do
+      field :root do
+        interface :person do
+          field(:name)
+        end
+      end
+    end
   end
 
   test "generate query from structure" do
-    assert Sample.query() == %{root: %{person: [name: nil]}}
+    assert Sample.query() == %{
+             root: %{
+               person: [name: nil]
+             }
+           }
   end
 end
