61177de658bc603f18bb2f01b8ff522b641eec95
danielefongo <danielefongo@gmail.com>
2021-03-13 18:00:12 +0100

    refactor tests

--- test/graphql_builder_test.exs
+++ test/graphql_builder_test.exs
@@ -3,93 +3,78 @@ defmodule GraphqlBuilderTest do
 
   describe "for inner body" do
     test "build empty" do
-      assert GraphqlBuilder.build(description(:people, %{}, %{})) =~ "{\n  }"
+      query = build_query(:people, %{}, %{})
+      assert query =~ "{\n  }"
     end
 
     test "build with root fields" do
-      assert GraphqlBuilder.build(description(:people, %{}, %{name: nil, surname: nil})) =~
-               "{\n    name\n    surname\n  }"
+      query = build_query(:people, %{}, %{name: nil, surname: nil})
+      assert query =~ "{\n    name\n    surname\n  }"
     end
 
     test "build with nested fields" do
-      assert GraphqlBuilder.build(description(:people, %{}, %{root: %{nested: nil}})) =~
-               "{\n    root {\n      nested\n    }\n  }"
+      query = build_query(:people, %{}, %{root: %{nested: nil}})
+      assert query =~ "{\n    root {\n      nested\n    }\n  }"
     end
 
     test "build with simple interfaces" do
-      assert GraphqlBuilder.build(description(:people, %{}, %{root: %{person: [name: nil]}})) =~
-               "{\n    root {\n      ... on Person {\n        name\n      }\n    }\n  }"
+      query = build_query(:people, %{}, %{root: %{person: [name: nil]}})
+      assert query =~ "{\n    root {\n      ... on Person {\n        name\n      }\n    }\n  }"
     end
 
     test "build with mixed fields and interfaces" do
-      assert GraphqlBuilder.build(
-               description(:people, %{}, %{root: %{value: nil, person: [name: nil]}})
-             ) =~
+      query = build_query(:people, %{}, %{root: %{value: nil, person: [name: nil]}})
+
+      assert query =~
                "{\n    root {\n      ... on Person {\n        name\n      }\n      value\n    }\n  }"
     end
 
     test "build using camel case" do
-      assert GraphqlBuilder.build(description(:people, %{}, %{full_name: nil})) =~
-               "{\n    fullName\n  }"
+      query = build_query(:people, %{}, %{full_name: nil})
+      assert query =~ "{\n    fullName\n  }"
     end
   end
 
   describe "for query params" do
     test "build empty" do
-      assert GraphqlBuilder.build(description(:people, %{}, %{})) =~ "people {"
+      query = build_query(:people, %{}, %{})
+      assert query =~ "people {"
     end
 
     test "build with root arguments" do
-      assert GraphqlBuilder.build(
-               description(:people, %{first_name: :string, last_name: :string}, %{})
-             ) =~
-               "people(firstName: $firstName, lastName: $lastName) {"
+      query = build_query(:people, %{first_name: :string, last_name: :string}, %{})
+      assert query =~ "people(firstName: $firstName, lastName: $lastName) {"
     end
 
     test "build with nested arguments" do
-      assert GraphqlBuilder.build(
-               description(
-                 :people,
-                 %{
-                   user: %{first_name: :string, last_name: :string}
-                 },
-                 %{}
-               )
-             ) =~
-               "people(user: {firstName: $firstName, lastName: $lastName}) {"
+      query = build_query(:people, %{user: %{first_name: :string, last_name: :string}}, %{})
+      assert query =~ "people(user: {firstName: $firstName, lastName: $lastName}) {"
     end
   end
 
   describe "for query" do
     test "build empty" do
-      assert GraphqlBuilder.build(description(:people, %{}, %{})) =~ "query {"
+      query = build_query(:people, %{}, %{})
+      assert query =~ "query {"
     end
 
     test "build with root arguments" do
-      assert GraphqlBuilder.build(
-               description(:people, %{first_name: :string, age: :integer}, %{})
-             ) =~
-               "query ($age: Integer, $firstName: String) {"
+      query = build_query(:people, %{first_name: :string, age: :integer}, %{})
+      assert query =~ "query ($age: Integer, $firstName: String) {"
     end
 
     test "build with nested arguments" do
-      assert GraphqlBuilder.build(
-               description(
-                 :people,
-                 %{foo: :integer, user: %{first_name: :string}},
-                 %{}
-               )
-             ) =~
-               "query ($foo: Integer, $firstName: String) {"
+      query = build_query(:people, %{foo: :integer, user: %{first_name: :string}}, %{})
+      assert query =~ "query ($foo: Integer, $firstName: String) {"
     end
   end
 
-  defp description(object, args, body) do
-    %{
+  defp build_query(object, args, body) do
+    GraphqlBuilder.build(%{
       kind: :query,
       object: object,
       arguments: args,
       body: body
-    }
+    })
   end
 end
