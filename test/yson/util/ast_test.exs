298eafa92886080adf946039f97a55c81727a8aa
danielefongo <danielefongo@gmail.com>
2021-06-02 14:41:44 +0200

    fix credo

--- test/yson/util/ast_test.exs
+++ test/yson/util/ast_test.exs
@@ -3,26 +3,29 @@ defmodule Yson.Util.ASTTest do
   alias Yson.Util.AST
 
   test "map single inner element" do
-    body = quote do
-      value(:field)
-    end
+    body =
+      quote do
+        value(:field)
+      end
 
     assert AST.fetch(body, [:value]) == [{:value, [], [:field]}]
   end
 
   test "map multiple inner elements" do
-    body = quote do
-      value(:field)
-      value(:field2)
-    end
+    body =
+      quote do
+        value(:field)
+        value(:field2)
+      end
 
     assert AST.fetch(body, [:value]) == [{:value, [], [:field]}, {:value, [], [:field2]}]
   end
 
   test "raise on invalid markers" do
-    body = quote do
-      value(:field)
-    end
+    body =
+      quote do
+        value(:field)
+      end
 
     assert_raise RuntimeError, fn ->
       AST.fetch(body, [])
@@ -30,12 +33,11 @@ defmodule Yson.Util.ASTTest do
   end
 
   test "remap markers" do
-    body = quote do
-      value(:field)
-    end
+    body =
+      quote do
+        value(:field)
+      end
 
     assert AST.fetch(body, [:value], %{value: :reference}) == [{:reference, [], [:field]}]
   end
-
-
 end
